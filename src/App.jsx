70c73dc224fca5d735add4ea2b81e996691dc0e4
Suraj Nanavare <surajrajaram20@gmail.com>
2023-09-24 01:42:20 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Note from "./note";
+import Notes from "./notes";
+
+function card(pr) {
+  return <Note key={pr.key} title={pr.title} content={pr.content} />;
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {Notes.map(card)}
       <Footer />
     </div>
   );
 }
+
 export default App;
--- src/note.jsx
+++ src/note.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-function note(){
-  
-  return <div className="note"> 
-    <h1>This is the title </h1>  
-    <p> This is the content </p>  
-  </div>;
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
 }
 
-export default note;
+export default Note;
